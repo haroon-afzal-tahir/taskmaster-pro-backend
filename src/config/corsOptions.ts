6bc1f6de5e0033294cfe4eb201ebe8fb4487b5c6
Haroon Tahir <haroontahirr100@gmail.com>
2024-04-04 20:24:10 +0500

    Refactor CORS options to include localhost in development environment

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,18 +2,15 @@ const ALLOWED_ORIGINS = [
   "https://taskmaster-pro-swart.vercel.app",
 ]
 
-if (process.env.NODE_ENV === "development") {
-  ALLOWED_ORIGINS.push("http://localhost:5173")
+function getProfilecorsOrigin() {
+  if (process.env.NODE_ENV !== 'production') {
+    ALLOWED_ORIGINS.push('http://localhost:5173')
+  }
+  console.log("Allowed Origins: ", ALLOWED_ORIGINS)
+  return ALLOWED_ORIGINS
 }
 
 export const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    if (ALLOWED_ORIGINS.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-  credentials: true,
+  origin: getProfilecorsOrigin(),
+  credentials: true
 }
