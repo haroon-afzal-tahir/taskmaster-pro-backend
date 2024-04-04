6601221893cab1bb764451c807a8866b99723233
Haroon Tahir <haroontahirr100@gmail.com>
2024-04-04 12:55:44 +0500

    Update email password in email.ts and add console log for OTP in AuthService.ts

--- lib/email.ts
+++ lib/email.ts
@@ -15,7 +15,7 @@ export class EmailLib {
         debug: false,
         auth: {
           user: process.env.EMAIL,
-          pass: process.env.EMAIL_PASSWORD,
+          pass: process.env.PASSWORD,
         }
       });
 
