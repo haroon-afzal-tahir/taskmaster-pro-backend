96a09a976f7dbb96f4c4411655914a632ebe0da7
Haroon Tahir <haroontahirr100@gmail.com>
2024-04-04 06:42:10 +0500

    Remove unnecessary code and console.log in TagController.getTags()

--- controllers/TagController.ts
+++ controllers/TagController.ts
@@ -4,7 +4,6 @@ import { Tag } from "../models";
 export class TagController {
   public static async getTags(req: Request, res: Response) {
     try {
-      const tags = await Tag.find();
       // find count of each tags present in the Task collection
       const tagCount = await Tag.aggregate([
         {
@@ -23,8 +22,6 @@ export class TagController {
           },
         },
       ]);
-
-      console.log(tagCount);
 
       return res.status(200).json(tagCount);
     } catch (error) {
